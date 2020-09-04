a2ca7a1b089de865a8099b42c9c87610aa2b5a1f
huangli <huanglixianhui@163.com>
2020-09-04 17:11:15 +0800

    change content

---
+++ BinaryTree/binarytree.h
@@ -0,0 +1,41 @@
+#pragma once
+#ifndef BITNARYTREE_H_
+#define BITNARYTREE_H_
+#include <iostream>
+#include <stack>
+#include <queue>
+
+typedef struct TreeNode* Bitree;
+typedef char ElemType;
+struct TreeNode
+{
+	ElemType data;
+	TreeNode* lchild;
+	TreeNode* rchild;
+};
+//创建树
+void CreateTree(Bitree& T);
+//销毁树
+void DestroyTree(Bitree& T);
+
+//递归遍历树
+void PreOrderTraverse(Bitree T);
+void InOrderTraverse(Bitree T);
+void AftOrderTraverse(Bitree T);
+
+//非递归遍历树
+void PreOrederTraverseNoRecurse(Bitree T);
+void InOrderTraverseNoRecurse(Bitree T);
+void AftOrderTraverseNoRecurse(Bitree T);
+
+void LayerTraverseNoRecurse(Bitree T);
+void PreorderLrchildTraverse(Bitree T);
+
+//输出叶子结点
+void TreeLeavesPreOrder(Bitree T);
+void TreeLeavesInOrder(Bitree T);
+void TreeLeavesAftOrder(Bitree T);
+
+//树的深度
+int TreeDepth(Bitree T);
+#endif // !BITNARYTREE_H_
---
+++ BinaryTree/tesetcase.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include "binarytree.h"
+
+int main()
+{
+	using namespace std;
+	Bitree Tree;
+	//create tree in pre order
+	cout << "create treee in pre order" << endl;
+	CreateTree(Tree);
+
+	//traverse trees in recurve way
+	cout << "\npre order traverse: ";
+	PreOrderTraverse(Tree);
+	cout<< "\nin order traverse: ";
+	InOrderTraverse(Tree);
+	cout << "\nAter order traverse: ";
+	AftOrderTraverse(Tree);
+	cout << endl;
+
+	//traverse tree in non recurse way
+	cout << "\nPreOrederTraverseNoRecurse:";
+	PreOrederTraverseNoRecurse(Tree);
+	cout << "\nInOrederTraverseNoRecurse:";
+	InOrderTraverseNoRecurse(Tree);
+	cout << "\nAftOrederTraverseNoRecurse:";
+	AftOrderTraverseNoRecurse(Tree);
+	cout << endl;
+
+	cout << "\nLayerTraverseNoRecurse:";
+	LayerTraverseNoRecurse(Tree);
+
+	//左右孩子法实现先序遍历
+	cout << "\nPreorderLrchildTraverse:";
+	PreorderLrchildTraverse(Tree);
+	cout << endl;
+
+	//叶子结点
+	cout << "\nget leaves in preorder:";
+	TreeLeavesPreOrder(Tree);
+	cout << "\nget leaves in inorder:";
+	TreeLeavesInOrder(Tree);
+	cout << "\nget leaves in postorder:" ;
+	TreeLeavesAftOrder(Tree);
+	cout << endl;
+
+	//获取树的深度
+	cout << "tree depth: "<<TreeDepth(Tree) << endl;
+
+	//销毁树
+	DestroyTree(Tree);
+
+	return 0;
+}
