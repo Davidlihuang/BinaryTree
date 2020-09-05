7bee9366c7593e91c414c6667b6d073c923bcca5
huangli <huanglixianhui@163.com>
2020-09-05 11:49:45 +0800

    update source file

--- BinaryTree/tesetcase.cpp
+++ BinaryTree/tesetcase.cpp
@@ -46,7 +46,22 @@ int main()
 
 	//获取树的深度
 	cout << "tree depth: "<<TreeDepth(Tree) << endl;
-
+	//树的结点数
+	int num = NodesOfTree(Tree);
+	cout << "node number = " << num << endl;
+	//树的度
+	int zero=0, one=0, two=0;
+	GetDegree(Tree, zero, one, two);
+	cout << "Trees total number of diffrent degrees:\n ";
+	cout << "zero degree nodes num:" << zero << endl;
+	cout << "one degree nodes num:" << one << endl;
+	cout << "two degree nodes num:" << two << endl<<endl;
+	//复制树
+	Bitree newTree;
+	CopyTree(newTree, Tree);
+	cout << "newTree: ";
+	PreOrderTraverse(newTree);
+	cout << endl<<endl;
 	//销毁树
 	DestroyTree(Tree);
 
