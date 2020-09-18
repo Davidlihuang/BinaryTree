98f5a0d531a4959ec425af448e6c7f4723c4615f
huangli <huanglixianhui@163.com>
2020-09-18 09:00:53 +0800

    last change

--- stack/main.cpp
+++ stack/main.cpp
@@ -8,32 +8,37 @@ int main() {
     for (int i=0;i<3;i++) {
         lstack.push(i+1);
     }
-    cout << lstack << endl;
+    cout <<"---------stackList--------\n初始化栈："<<lstack << endl;
 
     //copy Constructor
     ListStack<int> lstack1(lstack);
-    cout << lstack1 << endl;
+    cout <<"复制构造函数初始化栈："<< lstack1 << endl;
 
     //assigned 
     ListStack<int> lstack2;
     lstack2 = lstack1;
-    cout << "=: " << lstack2 << endl;
+    cout << "赋值：" << lstack2 << endl;
     
     //pop all element
+    int val = 0;
     while(lstack.size()) {
-        lstack.pop();
+        val = lstack.pop();
+        cout <<"pop:" <<val << "\t";
     }
+    cout << endl;
+    
     cout << lstack << endl;
 
     //sequenList
+    cout <<"---------sequenList--------\n初始化栈："<<lstack << endl;
     SequenStack<int> sstack(10);
     for(int i =0; i<10; i++) {
         sstack.push(i+1);
     }
-    cout <<"sstack size:"<<sstack.size()<< " elemet: "<<sstack << endl;
+    cout <<"sequen stack size: "<<sstack.size()<< " elemet: "<<sstack << endl;
     for(int i =0; i<11; i++) {
         sstack.pop();
-        cout <<"sstack size:"<<sstack.size()<< " elemet: "<<sstack << endl;   
+        cout <<"sequen stack size: "<<sstack.size()<< " elemet: "<<sstack << endl;   
     }
     cout << "test ended!" <<endl;
     return 0;
