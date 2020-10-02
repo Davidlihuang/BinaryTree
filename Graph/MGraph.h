813dec81c352ee9ebe7b2b92b3a4a3766d01ef0a
huangli <huanglixianhui@163.com>
2020-10-02 11:28:18 +0800

    最小生成树

--- Graph/MGraph.h
+++ Graph/MGraph.h
@@ -36,6 +36,12 @@ void DFS(MGraph* G, int i);    //深度优先遍历单一连通分量；
 void DFSTraverse(MGraph* G);   //深度优先遍历整个图的所有分量
 int InDegree(MGraph* G, int i); //获取任意结点的入度
 int OutDegree(MGraph* G, int i); //获取任意结点的出度
+void MiniSpanTree_Prim(MGraph* G); //最小生成树prim算法
+void MiniSpanTree_krusal(MGraph* G); //Krusal最小生成树算法
+
+typedef int PathRute[MaxVetex];
+typedef int ShortPathTable[MaxVetex];
+void ShortestPath_Dijsktra(MGraph* G, int v0, PathRute* p, ShortPathTable* D); //最短路径算法
 
 
 
