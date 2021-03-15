c7926f2bfa293f1f66758b75f541105c38878029
huangli-linux <huanglixianhui@163.com>
2021-03-15 23:21:45 +0800

    完善笔记

--- DSAInCpp/Algorithm/insertSort/insertSort.cpp
+++ DSAInCpp/Algorithm/insertSort/insertSort.cpp
@@ -1,19 +1,29 @@
 #include <iostream>
 using namespace std;
 
-
-//升序排列
+//插入排序
 template <typename T>
-void insertSort(T data[], int n)
+void insertSort(T data[], int n, bool opt)
 {
     T temp;
-    for (int i = 1, pos ; i < n; i++)
+    for (int i = 1, pos; i < n; i++)
     {
         temp = data[i];
-        for(pos = i ; pos > 0 && temp < data[pos-1];  pos--)
+        if (opt == true) //升序
         {
-            data[pos] = data[pos-1];
+            for (pos = i; pos > 0 && temp < data[pos - 1]; pos--)
+            {
+                data[pos] = data[pos - 1];
+            }
         }
+        else //降序
+        {
+            for (pos = i; pos > 0 && temp > data[pos - 1]; pos--)
+            {
+                data[pos] = data[pos - 1];
+            }
+        }
+
         data[pos] = temp;
     }
 }
@@ -31,11 +41,11 @@ void printList(T data[], int n)
 int main()
 {
     int data[]{2, 7, 8, 9, 7, 6, 56, 8, 56, 78, 65, 35};
-    int len = sizeof(data)/sizeof(int);
-    cout <<"origin: ";
+    int len = sizeof(data) / sizeof(int);
+    cout << "origin: ";
     printList(data, len);
-    insertSort(data, len);
-    cout <<"after sort: ";
+    insertSort(data, len, false);
+    cout << "after sort: ";
     printList(data, len);
     return 0;
 }
